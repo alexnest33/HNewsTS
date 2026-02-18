d856d116f6a7fb8aaa66c900772fe00a13da8e42
alex <alexnestx5@gmail.com>
2026-02-18 12:42:30 +0300

    Add GitHub Pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import store from './rtk/store/store.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './rtk/store/store.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-  <App />
+      <App />
     </Provider>
-  \
-  </StrictMode>,
-)
+  </StrictMode>
+);
