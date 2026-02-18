457102e721a8cbaa24f2dff7fd2f205615faa9c8
alex <alexnestx5@gmail.com>
2026-02-18 12:48:17 +0300

    Add BrowserRouter for GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router';
 import store from './rtk/store/store.tsx';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <BrowserRouter basename="/HNewsTS">
       <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
