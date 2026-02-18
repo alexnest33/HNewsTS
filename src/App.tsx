c3f2aa354bebb6629655981ab95b0e43d5817618
alex <alexnestx5@gmail.com>
2026-02-18 12:51:09 +0300

    fix router for GH pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Route,Routes } from 'react-router'
+import { Route,Routes } from 'react-router'
 import { LatestNewsPage } from './pages/LatestNewsPage'
 import NewsDetailsPage from './pages/NewsDetailsPage'
 import './App.css'
@@ -9,12 +9,10 @@ import './App.css'
 function App() {
   return (
     <>
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LatestNewsPage />} />
          <Route path="/news/:id" element={<NewsDetailsPage />} />
       </Routes>
-    </BrowserRouter>
     </>
   )
 }
